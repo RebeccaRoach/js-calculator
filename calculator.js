d3fbbc7af9c2224240e2b6d4d4dbf721b2a0f387
RebeccaRoach <rebeccaroach12@gmail.com>
2020-03-31 15:58:44 -0700

    Defined variables for four supported operations as simulated input data.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,26 @@
+// define constants to substitute user input
 const exampleAdditionInput = {
   num1: 3,
   num2: 5,
-  operation: 'add',
+  operation: ['add', '+']
 }
+
+const exampleSubtractionInput = {
+  num1: 55,
+  num2: 15,
+  operation: ['subtract', '-']
+}
+
+const exampleMultiplicationInput = {
+  num1: 4,
+  num2: 8,
+  operation: ['multiply', '*']
+}
+
+const exampleDivisionInput = {
+  num1: 100,
+  num2: 5,
+  operation: ['divide', '/']
+}
+
+//console.log(exampleDivisionInput['operation'][0]);
