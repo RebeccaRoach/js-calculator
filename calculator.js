f2c6345f1e4a81f9884e492da6074bd7b23c9b50
RebeccaRoach <rebeccaroach12@gmail.com>
2020-03-31 21:08:26 -0700

    Refactored isNumValid and cleaned for submission.

--- calculator.js
+++ calculator.js
@@ -1,25 +1,25 @@
 // define constants to simulate user input
 const exampleAdditionInput = {
-  num1: 1.4,
+  num1: 8,
   num2: 8.2,
   operation: ['add', '+']
 }
 
 const exampleSubtractionInput = {
-  num1: 27,
-  num2: 4,
+  num1: 28,
+  num2: -80,
   operation: ['subtract', '-']
 }
 
 const exampleMultiplicationInput = {
-  num1: 4,
-  num2: 8,
+  num1: 5,
+  num2: 9,
   operation: ['multiply', '*']
 }
 
 const exampleDivisionInput = {
   num1: 100,
-  num2: 20,
+  num2: 4,
   operation: ['divide', '/']
 }
 
@@ -32,21 +32,17 @@ const divide = (num1, num2) => num1 / num2;
 
 // helper method to validate input
 const isNumValid = function(input) {
-  if (typeof(input) !== 'number') {
-    return false;
-  } else {
-    return true;
-  }
+  return (typeof(input) === 'number' ? true : false);
 }
 
 
 // main calculator logic
 const calculate = function(input) {
-  // validate input nums
+  // validate two input nums
   if (!isNumValid(input.num1) || !isNumValid(input.num2)) {
-    console.log("Oops! Input must be a number.");
-    return;
+    return "Oops! Input must be a number.";
   } else {
+    let result = null;
     // only word and symbol allowed for each operation
     if (input['operation'].includes('add') || input['operation'].includes('+')) {
       result = add(input['num1'], input['num2']);
@@ -56,43 +52,17 @@ const calculate = function(input) {
         result = multiply(input['num1'], input['num2']);
     } else if (input['operation'].includes('divide') || input['operation'].includes('/')) {
       if (input['num2'] === 0) {
-        console.log('Oops! Cannot divide by zero.');
-        return;
+        return 'Oops! Cannot divide by zero.';
       } else {
         result = divide(input['num1'], input['num2']);
       }
     } else {
-      console.log('Oops! The operator must be a valid operation word or symbol.');
-      return;
+      return 'Oops! The operator must be a valid operation word or symbol.';
     }
 
     return `Your result: ${result}`
   }
 }
-
-
-//   switch (suppliedOperation) {
-//     case 'add':
-//     case '+':
-//       add(input['num1'], input['num2']);
-//       break;
-//     case 'subtract':
-//     case '-':
-//       result = num1 - num2;
-//       break;
-//     case 'multiply':
-//     case '*':
-//       result = num1 * num2;
-//       break;
-//     case 'divide':
-//     case '/':
-//       result = num1 / num2;
-//       break;
-//     default: 
-//       console.log('Sorry, did not understand operation.')
-//   }
-//   return `Your result: ${result}`;
-// }
 
 
 console.log(calculate(exampleAdditionInput));
